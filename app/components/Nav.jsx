a7736578e9b7cf0e787a88f7e783af0cae447d59
Keane Lumagui <keanelumagui@gmail.com>
2016-10-21 15:59:05 +0000

    style navigation

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,33 +1,31 @@
-var React   = require("react");
-var {Link, IndexLink}  = require("react-router");
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
 
-
-var Nav = React.createClass({
-   render: function () {
-       return (
-          <div className='top-bar'>
-            <div className='tob-bar-left'>
-                <ul className='menu'> 
-                    <li className='menu-text'>React Timer</li>
-                    <li>
-                        <IndexLink to='/' activeClassName='active-link'>Timer</IndexLink>
-                    </li>
-                    <li>
-                        <Link to='/' activeClassName='active-link'>Count Down</Link>    
-                    </li>
-                </ul>
-            </div>
-            
-            <div className='top-bar-right'>
-                <ul className='menu'>
-                    <li className='menu-text'>
-                        Created by: <a href='http://carlvincentbaydo.xyz' target='_blank'>Carl Vincent Baydo</a>
-                    </li>
-                </ul>
-            </div>
-          </div> 
-        );
-   } 
-});
+var Nav = () => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">
+            React Timer App
+          </li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+          </li>
+          <li>
+            <Link to="/" activeClassName="active-link">Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">
+            Created by <a href="http://carlvincentbaydo.xyz" target="_blank">Carl Vincent Baydo</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 module.exports = Nav;
